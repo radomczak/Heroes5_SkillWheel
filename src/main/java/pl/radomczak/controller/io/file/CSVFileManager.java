3b08e6304a93ff302aae81895467067eb811e360
Radoslaw Popielarski <radomczak015@o2.pl>
2021-03-30 12:30:55 +0200

    import exceptions

--- src/main/java/pl/radomczak/controller/io/file/CSVFileManager.java
+++ src/main/java/pl/radomczak/controller/io/file/CSVFileManager.java
@@ -1,8 +1,10 @@
 package pl.radomczak.controller.io.file;
 
 import pl.radomczak.model.*;
+import pl.radomczak.model.exception.AbilityNotFoundException;
 import pl.radomczak.model.exception.DataImportException;
 import pl.radomczak.model.exception.NoSuchRaceException;
+import pl.radomczak.model.exception.SkillNotFoundException;
 import pl.radomczak.repository.*;
 
 import java.io.BufferedReader;
@@ -20,7 +22,7 @@ public class CSVFileManager implements FileManager {
     private SkillsRepository skillsRepository;
 
     @Override
-    public Wheel importWheel() throws DataImportException {
+    public Wheel importWheel() {
         Wheel wheel = new Wheel();
         getRepositories(wheel);
 
@@ -39,7 +41,7 @@ public class CSVFileManager implements FileManager {
         this.skillsRepository = new SkillsRepository(wheel.getSkills());
     }
 
-    private void importSkills() {
+    private void importSkills(){
         String FILE_NAME = "Skills.csv";
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_NAME))) {
             bufferedReader.lines()
@@ -59,14 +61,27 @@ public class CSVFileManager implements FileManager {
         String name = data[0];
         String description = data[1];
         String image = data[2];
-        HashSet<Ability> requiredAbilities = getAbilitiesFromString(data[3]);
+
+        HashSet<Ability> requiredAbilities;
+        try {
+            requiredAbilities = getAbilitiesFromString(data[3]);
+        } catch (AbilityNotFoundException e) {
+            throw new DataImportException(e.getMessage());
+        }
+
         Race race;
         try {
             race = Race.createOptionFromString(data[4]);
         } catch (NoSuchRaceException ex) {
             throw new DataImportException(ex.getMessage());
         }
-        HashSet<Skill> skills = getSkillsFromString(data[5]);
+
+        HashSet<Skill> skills;
+        try {
+            skills = getSkillsFromString(data[5]);
+        } catch (SkillNotFoundException e) {
+            throw new DataImportException(e.getMessage());
+        }
 
         skill = Skill.builder()
                 .withName(name)
@@ -102,7 +117,13 @@ public class CSVFileManager implements FileManager {
         int proficiencyLevel = Integer.parseInt(data[3]);
         boolean racial = Boolean.parseBoolean(data[4]);
         HashSet<Race> races = getRacesFromString(data[5]);
-        HashSet<Ability> abilities = getAbilitiesFromString(data[6]);
+
+        HashSet<Ability> abilities;
+        try {
+            abilities = getAbilitiesFromString(data[6]);
+        } catch (AbilityNotFoundException e) {
+            throw new DataImportException(e.getMessage());
+        }
 
         ability = Ability.builder()
                 .withName(name)
@@ -139,10 +160,16 @@ public class CSVFileManager implements FileManager {
         Optional<Skill> s = skillsRepository.findByName(data[1]);
         if (s.isPresent()) uniqueSkill = s.get();
 
-        HashSet<Skill> startingSkills = getSkillsFromString(data[2]);
-        HashSet<Ability> startingAbilities = getAbilitiesFromString(data[3]);
-        Race race;
+        HashSet<Skill> startingSkills;
+        HashSet<Ability> startingAbilities;
+        try {
+            startingSkills = getSkillsFromString(data[2]);
+            startingAbilities = getAbilitiesFromString(data[3]);
+        } catch (SkillNotFoundException | AbilityNotFoundException e) {
+            throw new DataImportException(e.getMessage());
+        }
 
+        Race race;
         try {
             race = Race.createOptionFromString(data[4]);
         } catch (NoSuchRaceException ex) {
@@ -177,11 +204,19 @@ public class CSVFileManager implements FileManager {
         Build build;
 
         String name = data[0];
+
         Hero hero = null;
         Optional<Hero> h = heroesRepository.findByName(data[1]);
         if (h.isPresent()) hero = h.get();
-        HashSet<Skill> skillSet = getSkillsFromString(data[2]);
-        HashSet<Ability> abilitySet = getAbilitiesFromString(data[3]);
+
+        HashSet<Skill> skillSet;
+        HashSet<Ability> abilitySet;
+        try {
+            skillSet = getSkillsFromString(data[2]);
+            abilitySet = getAbilitiesFromString(data[3]);
+        } catch (SkillNotFoundException | AbilityNotFoundException e) {
+            throw new DataImportException(e.getMessage());
+        }
 
         build = Build.builder()
                 .withName(name)
@@ -193,42 +228,38 @@ public class CSVFileManager implements FileManager {
         return build;
     }
 
-    private HashSet<Ability> getAbilitiesFromString(String abilitiesString) {
-        if (abilitiesString.equals(""))
-            return null;
-        else {
+    private HashSet<Ability> getAbilitiesFromString(String abilitiesString) throws AbilityNotFoundException {
+        HashSet<Ability> abilities = new HashSet<>();
+        if (!abilitiesString.equals("")) {
             String[] data = abilitiesString.split(",");
-            HashSet<Ability> abilities = new HashSet<>();
             Optional<Ability> ability;
             for (String s : data) {
                 ability = abilitiesRepository.findByName(s);
-                ability.ifPresent(abilities::add);
+                if (ability.isPresent()) abilities.add(ability.get());
+                else throw new AbilityNotFoundException("Nie znaleziono umiejetnosci: " + s);
             }
-            return abilities;
         }
+        return abilities;
     }
 
-    private HashSet<Skill> getSkillsFromString(String skillsString) {
-        if (skillsString.equals(""))
-            return null;
-        else {
+    private HashSet<Skill> getSkillsFromString(String skillsString) throws SkillNotFoundException {
+        HashSet<Skill> skills = new HashSet<>();
+        if (!skillsString.equals("")) {
             String[] data = skillsString.split(",");
-            HashSet<Skill> skills = new HashSet<>();
             Optional<Skill> skill;
             for (String s : data) {
                 skill = skillsRepository.findByName(s);
-                skill.ifPresent(skills::add);
+                if (skill.isPresent()) skills.add(skill.get());
+                else throw new SkillNotFoundException("Nie znaleziono umiejetnosci: " + s);
             }
-            return skills;
         }
+        return skills;
     }
 
     private HashSet<Race> getRacesFromString(String raceString) {
-        if (raceString.equals(""))
-            return null;
-        else {
+        HashSet<Race> races = new HashSet<>();
+        if (!raceString.equals("")) {
             String[] data = raceString.split(",");
-            HashSet<Race> races = new HashSet<>();
             Race race;
             for (String s : data) {
                 try {
@@ -238,8 +269,8 @@ public class CSVFileManager implements FileManager {
                     throw new DataImportException(ex.getMessage());
                 }
             }
-            return races;
         }
+        return races;
     }
 
 }
