77703f3a44fdd9569bbe063eb5d7996de1bfb21d
Radoslaw Popielarski <radomczak015@o2.pl>
2021-04-14 10:18:13 +0200

    Testing classes

--- src/test/java/pl/radomczak/model/HeroTest.java
+++ src/test/java/pl/radomczak/model/HeroTest.java
@@ -5,8 +5,13 @@ import org.junit.*;
 import java.util.HashSet;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class HeroTest {
+    Hero hero;
+    Skill skill;
+    Ability ability;
+
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
     }
@@ -17,6 +22,31 @@ public class HeroTest {
 
     @Before
     public void setUp() throws Exception {
+        HashSet<Race> races = new HashSet<>();
+        races.add(Race.RYCERZ);
+        races.add(Race.LORD_DEMONOW);
+
+        ability = Ability.builder()
+                .withName("Test name")
+                .withDescription("description")
+                .withImage("image")
+                .withProficiencyLevel(2)
+                .withAllowedRaces(races)
+                .withRacial(false)
+                .withRequiredAbilities(new HashSet<>())
+                .build();
+
+        HashSet<Ability> abilities = new HashSet<>();
+        abilities.add(ability);
+
+        skill = Skill.builder()
+                .withName("Skill name")
+                .withDescription("Skill desc")
+                .withImage("skill.png")
+                .withRequiredAbilities(abilities)
+                .withRace(Race.BARBARZYNCA)
+                .withRequiredSkills(new HashSet<>())
+                .build();
     }
 
     @After
@@ -25,11 +55,36 @@ public class HeroTest {
 
     @Test
     public void builderTest() {
+        HashSet<Ability> abilities = new HashSet<>();
+        abilities.add(ability);
 
+        hero = Hero.builder()
+                .withName("Clavius Testus Maximus")
+                .withUniqueSkill(skill)
+                .withStartingSkills(new HashSet<>())
+                .withStartingAbilities(abilities)
+                .withRace(Race.RYCERZ)
+                .build();
+
+        assertEquals("Clavius Testus Maximus",hero.getName());
+        assertNotNull(skill);
+        assertNotNull(hero.getStartingSkills());
+        assertNotNull(hero.getStartingAbilities());
+        assertEquals(Race.RYCERZ,hero.getRace());
     }
 
     @Test
     public void toCSVTest() {
+        HashSet<Ability> abilities = new HashSet<>();
+        abilities.add(ability);
 
+        hero = Hero.builder()
+                .withName("Clavius Testus Maximus")
+                .withUniqueSkill(skill)
+                .withStartingSkills(new HashSet<>())
+                .withStartingAbilities(abilities)
+                .withRace(Race.RYCERZ)
+                .build();
+        assertEquals("Clavius Testus Maximus;Skill name;;Test name;RYCERZ;",hero.toCSV());
     }
 }
--- src/test/java/pl/radomczak/model/SkillTest.java
+++ src/test/java/pl/radomczak/model/SkillTest.java
@@ -5,10 +5,12 @@ import org.junit.*;
 import java.util.HashSet;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SkillTest {
 
     Skill skill;
+    Ability ability;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
@@ -20,6 +22,19 @@ public class SkillTest {
 
     @Before
     public void setUp() throws Exception {
+        HashSet<Race> races = new HashSet<>();
+        races.add(Race.RYCERZ);
+        races.add(Race.LORD_DEMONOW);
+
+        ability = Ability.builder()
+                .withName("Test name")
+                .withDescription("description")
+                .withImage("image")
+                .withProficiencyLevel(2)
+                .withAllowedRaces(races)
+                .withRacial(false)
+                .withRequiredAbilities(new HashSet<>())
+                .build();
     }
 
     @After
@@ -28,11 +43,40 @@ public class SkillTest {
 
     @Test
     public void builderTest() {
+        HashSet<Ability> abilities = new HashSet<>();
+        abilities.add(ability);
 
+        skill = Skill.builder()
+                .withName("Skill name")
+                .withDescription("Skill desc")
+                .withImage("skill.png")
+                .withRequiredAbilities(abilities)
+                .withRace(Race.BARBARZYNCA)
+                .withRequiredSkills(new HashSet<>())
+                .build();
+
+        assertEquals("Skill name",skill.getName());
+        assertEquals("Skill desc",skill.getDescription());
+        assertEquals("skill.png",skill.getImage());
+        assertNotNull(skill.getRequiredAbilities());
+        assertEquals(Race.BARBARZYNCA,skill.getRace());
+        assertNotNull(skill.getRequiredSkills());
     }
 
     @Test
     public void toCSVTest() {
+        HashSet<Ability> abilities = new HashSet<>();
+        abilities.add(ability);
 
+        skill = Skill.builder()
+                .withName("Skill name")
+                .withDescription("Skill desc")
+                .withImage("skill.png")
+                .withRequiredAbilities(abilities)
+                .withRace(Race.BARBARZYNCA)
+                .withRequiredSkills(new HashSet<>())
+                .build();
+
+        assertEquals("Skill name;Skill desc;skill.png;Test name;BARBARZYNCA;;",skill.toCSV());
     }
 }
